158654557a47ed9e32e4053691a7c8899ca03da3
umer-iq <umeritsolutions@gmail.com>
2022-08-24 21:21:02 +0500

    make it run

--- index.js
+++ index.js
@@ -66,9 +66,9 @@ return res.status(status).json({
 const PORT = process.env.PORT|| 8080
 //herokusetup
 if(process.env.NODE_ENV==="production"){
-  app.use(express.static('/vtubepk/client/build'));
+  app.use(express.static('/client/build'));
   
-  app.get("*",(req,res) =>{res.sendFile(path.join(__dirname +'/server/client/build/index.html'))})
+  app.get("*",(req,res) =>{res.sendFile(path.join(__dirname +'/client/build/index.html'))})
 }
 else{
   app.get('/',(req,res) => res.send("Please set to production"))
